ab60eb0c4a7fd754de79ddf37a60f14b3ea53ba5
PixelSupreme <pixel@pxs.li>
2016-11-15 20:50:19 +0100

    Added basic command line options handling.

--- Neptools/Neptools.cpp
+++ Neptools/Neptools.cpp
@@ -3,19 +3,98 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <boost/program_options.hpp>
 
 #include "PacArchive.h"
 
 using namespace neptools;
+using namespace std;
 
 namespace fs = boost::filesystem;
+namespace po = boost::program_options;
 
-int main()
+struct Options
 {
-    std::string file_name{ "c:\\test\\game00001.pac" };
-    PacArchive archive{ fs::path(file_name) };
-    archive.open();
-    cout << archive.print_info();
+    wstring game_dir;
+    wstring out_dir;
+};
+
+
+void print_help(po::options_description desc)
+{
+    cout << "\n\tUsage: neptools.exe <gamedir> <outdir>" << "\n\n";
+    cout << "  <gamedir>\t\tDirectory of the game you want to extract files from." << '\n';
+    cout << "  <outdir>\t\tOutput directory for extracted files." << "\n\n";
+    cout << desc << endl;
+}
+
+//Parse the command line
+//
+bool get_commandline(int argc, wchar_t** argv, Options options)
+{
+    try
+    {
+        po::options_description general("General options");
+        general.add_options()
+            ("help,h", "Display help message")
+            ;
+        po::options_description dirs("Input and output directories");
+        dirs.add_options()
+            ("gamedir", po::wvalue<wstring>(&options.game_dir)->value_name("dir"), "Directory of the game you want to extract files from.")
+            ("outdir", po::wvalue<wstring>(&options.out_dir)->value_name("dir"), "Output directory for extraced files.")
+            ;
+        po::options_description cmdline_options;
+        cmdline_options.add(general).add(dirs);
+
+        po::positional_options_description pos;
+        pos.add("gamedir", 1).add("outdir", 1);
+
+        po::variables_map vmap;
+        po::store(po::wcommand_line_parser(argc, argv).options(cmdline_options).positional(pos).run(), vmap);
+
+        if (vmap.count("help"))
+        {
+            print_help(general);
+            return false;
+        }
+
+        vmap.notify();
+        if (!vmap.count("gamedir"))
+        {
+            cout << "Error: no input directory specified\n";
+            print_help(general);
+            return false;
+        }
+        if (!vmap.count("outdir"))
+        {
+            cout << "Error: no output directory specified\n";
+            print_help(general);
+            return false;
+        }
+        return true;
+    }
+    catch (const exception& e)
+    {
+        wcout << e.what() << endl;
+        return false;
+    }
+}
+
+int wmain(int argc, wchar_t* argv[])
+{
+    // initialize char conversion from system locale
+    locale::global(locale("")); 
+
+    // Process command line
+    Options options{L"blah", L"blubb"};
+
+    {
+        if (!get_commandline(argc, argv, options))
+        {
+            return -1;
+        }
+    }
+
     return 0;
 }
 
