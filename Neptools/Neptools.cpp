6856dbd15b3519862bae330e001c260f8f417275
PixelSupreme <pixel@pxs.li>
2016-12-12 20:06:58 +0100

    Re-enabled logging.

--- Neptools/Neptools.cpp
+++ Neptools/Neptools.cpp
@@ -24,9 +24,10 @@ struct options
     string out_dir;
 };
 
+log::logger g_log;
 
-void print_help(po::options_description desc)
-{
+
+void print_help(po::options_description desc) {
     u8print("\n\tUsage: neptools.exe <gamedir> <outdir>\n\n");
     u8print("  <gamedir>\t\tDirectory of the game you want to extract files from.\n");
     u8print("  <outdir>\t\tOutput directory for extracted files.\n\n");
@@ -35,8 +36,7 @@ void print_help(po::options_description desc)
     u8print(buffer.str());
 }
 
-void parse_commandline(int argc, wchar_t** argv, options& options)
-{
+void parse_commandline(int argc, wchar_t** argv, options& options) {
     try
     {
         wstring gamedir;
@@ -68,120 +68,121 @@ void parse_commandline(int argc, wchar_t** argv, options& options)
         vmap.notify();
         if (!vmap.count("gamedir"))
         {
-            //g_log.fatal(L"No input directory set\n");
-            throw std::invalid_argument("No input directory set.");
+            g_log.fatal("No input directory set\n");
+            throw std::invalid_argument("No input directory set.\n");
         }
         if (!vmap.count("outdir"))
         {
             outdir = fs::absolute(fs::system_complete(argv[0]).parent_path()).generic_wstring();
-            //g_log.info(L"No output directory set. Using default: " + options.out_dir + L'\n');
+            g_log.info("No output directory set. Using default: " + narrow(outdir) + '\n');
         }
         options.game_dir = narrow(gamedir);
         options.out_dir = narrow(outdir);
     }
     catch (const exception& e)
     {
-        u8print(e.what());
+        u8error(e.what());
         exit(1);
     }
 }
 
-void scan_pac_files(fs::path in_path, vector<fs::path>& pac_files)
-{
-
+void scan_pac_files(const fs::path& in_path, vector<fs::path>& pac_files) {
     auto data_dir = in_path;
     data_dir.append(L"data");
-    //g_log.trace(L"Current data path is: " + data_dir.wstring() + L'\n');
-
+    
     if (!fs::exists(data_dir) || !fs::is_directory(data_dir))
     {
-        //g_log.fatal(L"Input directory does not contain \"data\" subdir.\n");
+        g_log.fatal("Input directory does not contain \"data\" subdir.\n");
         exit(1);
     }
 
-    //g_log.info(L"Scanning \"data\" subdir for PAC files.\n");
+    g_log.info("Scanning \"data\" subdir for PAC files.\n");
     for (auto&& current : fs::directory_iterator(data_dir))
     {
         if (current.path().extension().compare(L".pac") == 0)
         {
-            //g_log.trace(L"Current file: " + current.path().wstring() + L'\n');
+            g_log.trace("Found file: " + narrow(current.path().generic_wstring()) + '\n');
             pac_files.push_back(current.path());
         }
     }
-
+    int base_num = pac_files.size();
+    {
+        stringstream buf;
+        buf << "Finished scanning \"data\" subdir. Found " << base_num << " PAC files.\n";
+        g_log.trace(buf.str());
+    }
 
     auto dlc_base_dir = in_path;
-    dlc_base_dir.append("DLC");
+    dlc_base_dir.append(L"DLC");
     if (fs::exists(dlc_base_dir) && fs::is_directory(dlc_base_dir))
     {
-        //g_log.info(L"Scanning DLC subdir for PAC files\n");
+        g_log.info("Scanning DLC subdir for PAC files\n");
         for (auto&& dlc : fs::directory_iterator(dlc_base_dir))
         {
             for (auto&& current : fs::directory_iterator(dlc))
             {
                 if (current.path().extension().compare(L".pac") == 0) {
-                    //g_log.trace(L"Current files: " + current.path().wstring() + L'\n');
+                    g_log.trace("Found file: " + narrow(current.path().generic_wstring()) + '\n');
                     pac_files.push_back(current.path());
                 }
             }
         }
-    }
-    else
+        {
+            stringstream buf;
+            buf << "Finished scanning \"DLC\" subdir. Found " << pac_files.size() - base_num << " PAC files\n";
+            g_log.trace(buf.str());
+        }
+    } else
     {
-        //g_log.info(L"DLC directory not found. Skipping\n");
+        g_log.info("DLC directory not found. Skipping\n");
     }
 
-    stringstream buffer;
-    buffer << "Finished scanning for PAC files. Found " << pac_files.size() << " files.\n";
-    //g_log.info(buffer.str());
+    stringstream buf;
+    buf << "Finished scanning PAC files. Found " << pac_files.size() << " files\n";
+    g_log.info(buf.str());
 }
 
-int wmain(int argc, wchar_t* argv[])
-{
-    // initialize locales
-    //locale::global(locale("en_US.UTF-8"));
-    //wcout.imbue(locale(""));
-
-    //g_log.set_logfile(L"nep.log");
-    //g_log.set_level(Log::LogLevel::Trace);
+int wmain(int argc, wchar_t* argv[]) {
+    g_log.set_logfile("nep.log");
+    g_log.set_level(log::log_level::trace);
 
     // Process command line
-    options options{ "", "" };
+    options options { "", "" };
     parse_commandline(argc, argv, options);
 
-    fs::path in_path(options.game_dir);
-    //g_log.trace(L"Input directory: " + in_path.wstring() + L'\n');
+    fs::path in_path(widen(options.game_dir));
+    g_log.trace("Input directory: " + narrow(in_path.generic_wstring()) + '\n');
     if (!exists(in_path))
     {
-        //g_log.fatal(L"Input directory doesn't exist.\n");
+        g_log.fatal("Input directory doesn't exist.\n");
         exit(1);
     }
     if (!is_directory(in_path))
     {
-        //g_log.fatal(L"Input directory is not a directory.\n");
+        g_log.fatal("Input directory is not a directory.\n");
         exit(1);
     }
     vector<fs::path> pac_files;
     scan_pac_files(in_path, pac_files);
-    
+
     vector<pac> pacs;
     pacs.reserve(pac_files.size());
-    pac arc;
+    pac archive;
     for (auto current : pac_files)
     {
-        arc.open(current.string());
-        pacs.push_back(arc);
-        arc.reset();
+        archive.open(current.string());
+        pacs.push_back(archive);
+        archive.reset();
     }
 
-    //g_log.trace(L"Generating CVS data.\n");
+    g_log.trace("Generating CVS data.\n");
     fs::ofstream pac_csv("pac.csv", ios_base::binary);
     fs::ofstream index_csv("index.csv", ios_base::binary);
     for (auto current : pacs)
     {
-        //g_log.trace("Current PAC file: " + current.get_filename() + '\n');
-        pac_csv << current.header_csv();
-        index_csv << current.index_csv();
+        g_log.trace("Current PAC file: " + current.get_filename() + '\n');
+        current.header_csv(pac_csv);
+        current.index_csv(index_csv);
     }
     return 0;
 }
